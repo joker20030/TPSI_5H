2af886dd5e7a1ea94e3f6a481241aab07fd3d650
jolly <forleostefano482@gmail.com>
2022-12-08 10:29:46 +0000

    update

--- B-ES03-03-ServerWeb/network/TcpServer.java
+++ B-ES03-03-ServerWeb/network/TcpServer.java
@@ -1,100 +1,100 @@
 /*
 Implementazione di un server web utilizzando la comunicazione tramite socket.
-Lettura dati multi riga provenienti dal cliente
+Lettura dati multi riga provenienti dal client
 cd Es03/ServerWeb-OnOff/src/network
 java TcpServer.java
 */
  
- rete pacchetto ;
+package network;
 
-importa  java . io .*;
-importa  java . netto . ServerSocket ;
-importa  java . netto . Presa ;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
- classe  pubblica TcpServer {
-	public  static  void  main ( String [] args ) lancia  Exception {
+public class TcpServer {
+	public static void main(String[] args) throws Exception {
 		
-		final  int  SERVER_PORT = 8765 ;
-		String  clientMsg = "" ;
-		Stringa  serverMsg = "" ;
-		 flag booleano ;
-		// URL url = nuovo URL("https://www.favicon.cc/");
+		final int SERVER_PORT=8765;
+		String clientMsg = "";
+		String serverMsg = "";
+		boolean flag;
+		// URL url = new URL("https://www.favicon.cc/");
 		// favicon.ico serverMsg += url;
 
-		prova {			 
+		try {			 
 			// Creazione del socket sul server e ascolto sulla porta
-			ServerSocket  serverSocket = new  ServerSocket ( SERVER_PORT );
-			Sistema . fuori . println ( "Server: in ascolto sulla porta " + SERVER_PORT );
+			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+			System.out.println("Server: in ascolto sulla porta " + SERVER_PORT);
 
-			bandiera = falso ;
-			while (! flag ) {
+			flag=false;
+			while(!flag) {
 				// Attesa della connessione con il client
-				Sistema . fuori . println ( "\nAttesa ricezione dati dal client ....................... \n" );
-				Socket  clientSocket = serverSocket . accettare ();
+				System.out.println("\nAttesa ricezione dati dal client ....................... \n");
+				Socket clientSocket = serverSocket.accept();
 				
-				// Crea un flusso di output per scrivere i dati e un flusso di input per leggere i dati dal socket
-				DataOutputStream  outStream = nuovo  DataOutputStream ( clientSocket . getOutputStream ());	
-				BufferedReader  inStream = nuovo  BufferedReader ( nuovo  InputStreamReader ( clientSocket . getInputStream ()));
+				// Create output stream to write data and input stream to read data from socket
+				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
+				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 				
-                //Lettura dati dal client un diritto alla volta   
-                clientMsg = inStream . readLine ();
-				Sistema . fuori . println ( clientMsg );	
+                //Lettura dati dal client un righa alla volta   
+                clientMsg=inStream.readLine();
+				System.out.println(clientMsg);	
 		 
-                // Elaborare qui i dati ricevuti dal client
+                // Elaborare qui i dati ricevuti dal client 
 
-                clientMsg . tagliare ();	//tolgo gli spazi all'inizio e alla fine della stringa
-				Stringa  arrayCliMsg []= clientMsg . diviso ( "\\s+" );
+                clientMsg.trim();	//tolgo gli spazi all'inizio e alla fine della stringa
+				String arrayCliMsg[]=clientMsg.split("\\s+");
 
 				//Invio dei dati su stream di rete al client
-				serverMsg = "HTTP/1.1 200 OK\r\n" ;
-				//serverMsg += "Connessione: chiudi\r\n";
-				serverMsg += "Tipo di contenuto: testo/html\r\n" ;
-                serverMsg += "\r\n" ;
+				serverMsg = "HTTP/1.1 200 OK\r\n";
+				//serverMsg += "Connection: close\r\n";
+				serverMsg += "Content-Type: text/html\r\n"; 
+                serverMsg += "\r\n";
 				
-                interruttore ( arrayCliMsg [ 1 ]) {
+                switch(arrayCliMsg[1]) {
 
-				    caso  "/" :
-						serverMsg += "<b><h2>Saluti da Marco Zambelan!!</h2></b>" ;
-						serverMsg += "<b><h3>Digita 'info' per vedere i comandi disponibili." ;
-                        rompere ;
+				    case "/":
+						serverMsg += "<b><h2>Saluti da Marco Zambelan!!</h2></b>";
+						serverMsg += "<b><h3>Digita 'info' per vedere i comandi disponibili.";
+                        break;
 
-					caso  "/info" :
-						serverMsg += "<b><h2>Comandi disponibili:</h2></b>" ;
-						serverMsg += "<b><h3>'on' : accende le luci;</h3></b>" ;
-						serverMsg += "<b><h3>'off' : spegne le luci;</h3></b>" ;
-						serverMsg += "<b><h3>'esci' : esce dal server;</h3></b>" ;
-						rompere ;
+					case "/info":
+						serverMsg += "<b><h2>Comandi disponibili:</h2></b>";
+						serverMsg += "<b><h3>'on' : accende le luci;</h3></b>";
+						serverMsg += "<b><h3>'off' : spegne le luci;</h3></b>";
+						serverMsg += "<b><h3>'quit' : esce dal server;</h3></b>";
+						break;
 
-                    maiuscole e minuscole  "/on" :
-						serverMsg += "<b><h2>Luci accese</h2></b>" ;
-                        rompere ;
+                    case "/on":
+						serverMsg += "<b><h2>Luci accese</h2></b>";
+                        break;
 
-                    caso  "/off" :
-						serverMsg += "<b><h2>Luci spente</h2></b>" ;
-                        rompere ;
+                    case "/off":
+						serverMsg += "<b><h2>Luci spente</h2></b>";
+                        break;
 
-					case  "/esci" :
-						serverMsg += "<b><h2>Uscita dal server...</h2></b>" ;
-						contrassegno = vero ;
-                        rompere ;
+					case "/quit":
+						serverMsg += "<b><h2>Uscita dal server...</h2></b>";
+						flag = true;
+                        break;
 
-                    default : serverMsg += "<b><h2>Errore</h2></b>" ;
+                    default : serverMsg += "<b><h2>Errore</h2></b>";
                 }
-            	Sistema . fuori . stampa ( serverMsg + "\n" );		
-                outstream . scrivere ( serverMsg . getBytes ());
-				outstream . filo ();
+            	System.out.print(serverMsg + "\n");		
+                outStream.write(serverMsg.getBytes());
+				outStream.flush();
 				
-				// Chiudi le risorse
-				clientSocket . chiudere ();
-				inStream . chiudere ();
-				outstream . chiudere ();
+				// Close resources
+				clientSocket.close();
+				inStream.close();
+				outStream.close();
 			}
-			Sistema . fuori . println ( "\n.......................Fine ricezione dati\n" );
-			// Chiudi le risorse
-			serverSocket . chiudere ();
+			System.out.println("\n....................... Fine ricezione dati\n");
+			// Close resources
+			serverSocket.close();
 
-		} catch ( Eccezione  e ) {
-			Sistema . fuori . println ( e );
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 	}
 }
